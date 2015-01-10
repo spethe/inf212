6cd98bd05441e7178dca8b2c418f32dc8057aa0c
Swanand Pethe <swanand.pethe@gmail.com>
2015-01-10 07:38:12 +0000

    Introduced smaller functions making it more readable and concise

--- Project1/src/TermFrequency.java
+++ Project1/src/TermFrequency.java
@@ -9,26 +9,22 @@ import java.util.*;
 public class TermFrequency {
 
     public static void main(String[] args) throws FileNotFoundException {
-        List<String> stopWordList = new ArrayList<String>();
+        
         Map<String, Integer> termFreqMap = new HashMap<String, Integer>();
+        
+        //Prepare Stop Words list
+        List<String> stopWordList = extractStopWords();
+        
+        //Read the file given as an input
+        Scanner corpusFileScanner = new Scanner(new File(args[0]));     
 
-
-        Scanner stopWordFileScanner = new Scanner(new File("../stop_words.txt")).useDelimiter(",");
-        while(stopWordFileScanner.hasNext()){
-            stopWordList.add(stopWordFileScanner.next());
-        }
-
-        Scanner corpusFileScanner = new Scanner(new File("pride-and-prejudice.txt"));
-
+        //Scan the input file for term frequency
         while(corpusFileScanner.hasNext()){
             String term = corpusFileScanner.next().toLowerCase();
-            String key;
-            Integer value;
-
             char[]termArray = term.toCharArray();
-
             StringBuilder newTermBuilder = new StringBuilder();
             String newTerm ="";
+            
             for(int i=0, j=0;i<termArray.length;i++){
                 if(Character.isLetter(termArray[i])){
                     newTerm = newTermBuilder.append(termArray[i]).toString();
@@ -41,6 +37,7 @@ public class TermFrequency {
                     }else continue;
                 }
             }
+            
             if(newTerm.length()>1){
                 populateFrequencyFor(newTerm,stopWordList ,termFreqMap);
             }
@@ -57,11 +54,21 @@ public class TermFrequency {
 
     }
 
+    private static List<String> extractStopWords() throws FileNotFoundException{
+        List<String> stopWordList = new ArrayList<String>();
+        Scanner stopWordFileScanner = new Scanner(new File("../stop_words.txt")).useDelimiter(",");
+        while(stopWordFileScanner.hasNext()){
+            stopWordList.add(stopWordFileScanner.next());
+        }
+        return stopWordList;
+    }
+    
     private static void populateFrequencyFor(String term, List<String> stopWordList,Map<String,Integer> termFreqMap){
         if(!stopWordList.contains(term)){
             Integer freq = termFreqMap.containsKey(term) ? termFreqMap.put(term, (termFreqMap.get(term)) + 1) : termFreqMap.put(term, 1);
         }
     }
+    
     static class ValueComparator implements Comparator<String> {
 
         Map<String, Integer> base;
@@ -69,13 +76,12 @@ public class TermFrequency {
             this.base = base;
         }
 
-        // Note: this comparator imposes orderings that are inconsistent with equals.
         public int compare(String a, String b) {
             if (base.get(a) >= base.get(b)) {
                 return -1;
             } else {
                 return 1;
-            } // returning 0 would merge keys
+            }
         }
     }
 }
